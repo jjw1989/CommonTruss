53841e69058efeda88bfb41e5179a7d9551fd696
sundy <sundy.jiang@powervision.me>
2017-08-29 22:41:34 +0800

    路由限制

--- app/src/main/java/com/powervision/gcs/app/PVApplication.kt
+++ app/src/main/java/com/powervision/gcs/app/PVApplication.kt
@@ -17,11 +17,11 @@ class PVApplication: BaseApplication() {
      * 初始化路由管理
      */
     private fun initARouter() {
-        if (BuildConfig.DEBUG) {       // 这两行必须写在init之前，否则这些配置在init过程中将无效
-            ARouter.openLog();         // 打印日志
-            ARouter.openDebug();       // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-            ARouter.printStackTrace(); // 打印日志的时候打印线程堆栈
+        if (true) {       // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog()          // 打印日志
+            ARouter.openDebug()        // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+            ARouter.printStackTrace()  // 打印日志的时候打印线程堆栈
         }
-        ARouter.init(this); // 尽可能早，推荐在Application中初始化
+        ARouter.init(this)  // 尽可能早，推荐在Application中初始化
     }
 }
