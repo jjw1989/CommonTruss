5677634088bf81a4962558861da7bd33d745282d
sundy <sundy.jiang@powervision.me>
2017-08-31 01:30:20 +0800

    add view

---
+++ app/src/main/java/com/powervision/gcs/view/PVTopMenuView.kt
@@ -0,0 +1,15 @@
+package com.powervision.gcs.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+/**
+ * 顶部视图
+ * Created by David on 2017/8/31.
+ */
+class PVTopMenuView(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
+    init {
+
+    }
+}
