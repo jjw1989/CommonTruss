e4f8103a8adbe08d6291bc56dce2a112ce90ef20
sundy <sundy.jiang@powervision.me>
2017-09-06 00:03:40 +0800

    添加网络日志

--- library/network/src/main/java/com/powervision/gcs/RetrofitManager.kt
+++ library/network/src/main/java/com/powervision/gcs/RetrofitManager.kt
@@ -1,7 +1,10 @@
 package com.powervision.gcs
 
 import com.powervision.gcs.config.ApiUrl
+import com.powervision.gcs.utils.JsonUtil
+import com.powervision.gcs.utils.LogUtil
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.jackson.JacksonConverterFactory
@@ -19,11 +22,15 @@ class RetrofitManager {
      * 初始化资源
      */
     init {
+        LogUtil.init(true)
         if (okHttpClient==null){
+            val logInterceptor = HttpLoggingInterceptor(HttpLogger())
+            logInterceptor.level = HttpLoggingInterceptor.Level.BODY
             okHttpClient=OkHttpClient().newBuilder()
                     .readTimeout(10000,TimeUnit.MILLISECONDS)
                     .writeTimeout(10000,TimeUnit.MILLISECONDS)
                     .connectTimeout(10000,TimeUnit.MILLISECONDS)
+                    .addNetworkInterceptor(logInterceptor)
                     .build()
         }
 
@@ -55,5 +62,25 @@ class RetrofitManager {
         }
     }
 
+    private inner class HttpLogger : HttpLoggingInterceptor.Logger {
+        private val mMessage = StringBuilder()
+
+        override fun log(message: String) {
+            var message = message
+            // 请求或者响应开始
+            if (message.startsWith("--> POST")) {
+                mMessage.setLength(0)
+            }
+            // 以{}或者[]形式的说明是响应结果的json数据，需要进行格式化
+            if (message.startsWith("{") && message.endsWith("}") || message.startsWith("[") && message.endsWith("]")) {
+                message = JsonUtil.formatJson(message)
+            }
+            mMessage.append(message + "\n")
+            // 请求或者响应结束，打印整条日志
+            if (message.startsWith("<-- END HTTP")) {
+                LogUtil.d(mMessage.toString())
+            }
+        }
+    }
 
 }
