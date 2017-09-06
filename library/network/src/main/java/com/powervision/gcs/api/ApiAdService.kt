90d3715b1a2439c37280dd4004ed1a3734ecb2de
sundy <2436668590@qq.com>
2017-09-06 19:46:43 +0800

    add net

--- library/network/src/main/java/com/powervision/gcs/api/ApiAdService.kt
+++ library/network/src/main/java/com/powervision/gcs/api/ApiAdService.kt
@@ -1,8 +1,18 @@
 package com.powervision.gcs.api
 
+import com.powervision.gcs.bean.AdModel
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * 广告页接口
  * Created by Sundy on 2017/9/6.
  */
 interface ApiAdService {
+    /**
+     * 获取广告页
+     */
+    @GET("")
+    fun  getAdInfo(@Query("ip") ip:String):Observable<AdModel>
 }
