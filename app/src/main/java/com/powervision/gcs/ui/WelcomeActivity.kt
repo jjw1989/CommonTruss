d51173ff90560bbd4db9959e187004e0a058f5da
sundy <sundy.jiang@powervision.me>
2017-09-12 00:47:22 +0800

    图片处理

--- app/src/main/java/com/powervision/gcs/ui/WelcomeActivity.kt
+++ app/src/main/java/com/powervision/gcs/ui/WelcomeActivity.kt
@@ -3,34 +3,22 @@ package com.powervision.gcs.ui
 import android.animation.Animator
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
-import android.icu.util.TimeUnit
 import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.util.Log
 import android.view.View
 import android.view.animation.AccelerateInterpolator
+import com.alibaba.android.arouter.launcher.ARouter
 import com.powervision.gcs.R
-import com.powervision.gcs.RetrofitManager
-import com.powervision.gcs.api.ApiAdService
 import com.powervision.gcs.base.BaseActivity
-<<<<<<< HEAD
-import com.powervision.gcs.peimissions.PermissionRequest
-import com.powervision.gcs.utils.LogUtil
-import io.reactivex.*
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.internal.util.HalfSerializer.onNext
-import io.reactivex.schedulers.Schedulers
-=======
 import com.powervision.gcs.config.PVFileDir
 import com.powervision.gcs.utils.TFileUtil
 import com.yanzhenjie.permission.AndPermission
 import com.yanzhenjie.permission.Permission
 import com.yanzhenjie.permission.PermissionNo
 import com.yanzhenjie.permission.PermissionYes
->>>>>>> f683a358151577515462e24eae4ba1121c84beff
 import kotlinx.android.synthetic.main.gcs_welcome_layout.*
-import java.util.*
 
 
 /**
@@ -82,21 +70,6 @@ class WelcomeActivity : BaseActivity() {
      * 权限请求
      */
     private fun permissionsRequest() {
-<<<<<<< HEAD
-      var requestPermissons=PermissionRequest(this,object :PermissionRequest.PermissionCallback{
-          override fun onSuccessful() {
-             Toast.makeText(this@WelcomeActivity,"111111111111111111111111",Toast.LENGTH_LONG).show()
-              LogUtil.i("1111111111111111111111111111111111111")
-              initNetWork()
-          }
-
-          override fun onFailure() {
-              LogUtil.i("22222222222222222222222222222222222222")
-          }
-
-      })
-        requestPermissons.request()
-=======
         AndPermission.with(this)
                 .requestCode(300)
                 .permission(
@@ -112,15 +85,6 @@ class WelcomeActivity : BaseActivity() {
 //        maps.put(Params.KEY_CHARSET,"zh")
 //        maps.put(Params.KEY_VERSION,Params.version)
 //        maps.put(Params.KEY_PARAMS,activationParams)
-
->>>>>>> f683a358151577515462e24eae4ba1121c84beff
-    }
-
-    private fun initNetWork() {
-
-//       RetrofitManager.getInstance().createService(ApiAdService::class.java).getAdInfo("").subscribeOn(Schedulers.io())
-//               .unsubscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).su
-
 
     }
 
@@ -141,7 +105,7 @@ class WelcomeActivity : BaseActivity() {
             override fun onAnimationEnd(p0: Animator?) {
                 //ARouter.getInstance().build("/set/plane").withTransition(R.anim.push_right_in,R.anim.push_right_out).navigation()
             //    ARouter.getInstance().build("/test/permission").withTransition(R.anim.push_right_in,R.anim.push_right_out).navigation()
-                // ARouter.getInstance().build("/ui/main").withTransition(R.anim.push_right_in,R.anim.push_right_out).navigation()
+                ARouter.getInstance().build("/ui/main").withTransition(R.anim.push_right_in,R.anim.push_right_out).navigation()
 //                ARouter.getInstance()
 //                        .build("/aty/fly/flight")
 //                        .withTransition(R.anim.push_right_in,R.anim.push_right_out)
@@ -166,8 +130,6 @@ class WelcomeActivity : BaseActivity() {
 
     @PermissionYes(300)
     private fun getPermissionYes(grantedPermissions: List<String>) {
-        // Successfully.
-        Log.i("qazx", "111111111111111111111"+grantedPermissions.toString())
         createDir()
 
     }
@@ -185,14 +147,8 @@ class WelcomeActivity : BaseActivity() {
 
     @PermissionNo(300)
     private fun getPermissionNo(deniedPermissions: List<String>) {
-        // Failure.
-        Log.i("qazx", "222222222222222222222222"+deniedPermissions.toString())
         if (AndPermission.hasAlwaysDeniedPermission(this, deniedPermissions)) {
-//            AndPermission.with(this)
-//                    .requestCode(300)
-//                    .permission(
-//                            Permission.SENSORS,Permission.LOCATION)
-//                    .callback(this).start()
+
         }
     }
 
---
+++ library/common/src/main/java/com/powervision/gcs/view/largeimage/factory/BitmapDecoderFactory.java
@@ -0,0 +1,9 @@
+package com.powervision.gcs.view.largeimage.factory;
+
+import android.graphics.BitmapRegionDecoder;
+
+import java.io.IOException;
+
+public interface BitmapDecoderFactory {
+    BitmapRegionDecoder made() throws IOException;
+}
